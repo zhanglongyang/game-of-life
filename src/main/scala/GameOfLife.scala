563d0cea3780e18569e70cf48ead946c21e511e8
Longyang Zhang <zhanglongyang.cn@gmail.com>
2017-01-17 21:05:57 +1100

    add neighbours method for location

--- src/main/scala/GameOfLife.scala
+++ src/main/scala/GameOfLife.scala
@@ -1,7 +1,7 @@
 import com.thoughtworks.binding.Binding.Var
 import com.thoughtworks.binding.{Binding, dom}
-import org.scalajs.dom.document
-import org.scalajs.dom.html.Table
+import org.scalajs.dom.{Event, document}
+import org.scalajs.dom.html.{Button, Table}
 
 import scala.scalajs.js.JSApp
 import scala.scalajs.js.annotation.JSExport
@@ -13,19 +13,36 @@ object GameOfLife extends JSApp {
     def isNeighbourOf(loc: Location): Boolean = {
       (Math.abs(this.x - loc.x) <= 1 && Math.abs(this.y - loc.y) <= 1) && !(this.equals(loc))
     }
+
+    def isValid: Boolean = {
+      (this.x > 0 && this.x < size) && (this.y > 0 && this.y < size)
+    }
+
+    def neighbours: List[Location] = {
+      List(
+        Location(x - 1, y - 1),
+        Location(x - 1, y),
+        Location(x - 1, y + 1),
+        Location(x, y - 1),
+        Location(x, y + 1),
+        Location(x + 1, y - 1),
+        Location(x + 1, y),
+        Location(x + 1, y + 1)
+      ).filter(_.isValid)
+    }
   }
 
-  case class Cell(status: Var[String], location: Location) {
+  case class Cell(status: Var[String]) {
     def isAlive: Boolean = this.status.get.eq("1")
   }
 
-  case class World(cells: List[Cell])
+  case class World(cellsMap: List[(Location, Cell)])
 
   val size = 20
 
   val data = World(({
     for (i <- 0 to size - 1; j <- 0 to size - 1) yield {
-      Cell(Var(randomStatus), Location(i, j))
+      (Location(i, j),  Cell(Var(randomStatus)))
     }
   }).toList)
 
@@ -33,16 +50,15 @@ object GameOfLife extends JSApp {
     Random.nextInt(7).toString
   }
 
-  @dom
-  def world: Binding[Table] = {
+  @dom def world: Binding[Table] = {
     <table border="1" cellPadding="5">
       {
         import scalaz.std.list._
-        data.cells.grouped(size).toList.map(row => {
+        data.cellsMap.grouped(size).toList.map(row => {
           <tr>
             {
-              row.map(cell => {
-                <td style={s"width: 20px; height: 20px; ${if (cell.status.get.eq("1")) "background-color: black"}"}></td>
+              row.map(cellMap => {
+                <td style={s"width: 20px; height: 20px; ${if (cellMap._2.status.get.eq("1")) "background-color: black"}"}></td>
               })
             }
           </tr>
