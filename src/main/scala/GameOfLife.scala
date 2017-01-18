f6ee8e9cbf8e674e28f09579e233432a73fc04e6
Longyang Zhang <zhanglongyang.cn@gmail.com>
2017-01-18 20:27:22 +1100

    evolve method starts to work

--- src/main/scala/GameOfLife.scala
+++ src/main/scala/GameOfLife.scala
@@ -1,7 +1,7 @@
 import com.thoughtworks.binding.Binding.Var
 import com.thoughtworks.binding.{Binding, dom}
-import org.scalajs.dom.document
-import org.scalajs.dom.html.Table
+import org.scalajs.dom.{Event, document}
+import org.scalajs.dom.html.{Button, Div, Table}
 
 import scala.collection.immutable.TreeMap
 import scala.scalajs.js.JSApp
@@ -54,8 +54,6 @@ object GameOfLife extends JSApp {
     }
   }): _*))
 
-  for (row <- data.cellsMap.grouped(size).toList) yield println(row)
-
   def randomStatus: String = {
     Random.nextInt(7).toString
   }
@@ -68,7 +66,7 @@ object GameOfLife extends JSApp {
           <tr>
             {
               for (cellMap <- row.toList) yield {
-                <td style={s"width: 20px; height: 20px; ${if (cellMap._2.status.get.eq("1")) "background-color: black"}"}></td>
+                <td style={s"width: 20px; height: 20px; ${if (cellMap._2.status.bind.eq("1")) "background-color: black"}"}></td>
               }
             }
           </tr>
@@ -77,9 +75,28 @@ object GameOfLife extends JSApp {
     </table>
   }
 
+  @dom def evolve: Binding[Button] = {
+    <button onclick={e: Event => {
+      for (
+        (loc, cell) <- data.cellsMap
+//        if loc.neighbours.filter(data.cellsMap.get(_).get.status.eq("1")).size > 0
+      ) yield {
+        println(loc + "|" + cell.status.get)
+        cell.status := "0"
+      }
+    }}>Evolve</button>
+  }
+
+  @dom def render: Binding[Div] = {
+    <div>
+      {world.bind}
+      {evolve.bind}
+    </div>
+  }
+
   @JSExport
   override def main(): Unit = {
-    dom.render(document.body, world)
+    dom.render(document.body, render)
   }
 }
 
