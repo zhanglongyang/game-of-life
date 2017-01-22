9fe45a390898f30a9a827227d787ecda011123ed
Longyang Zhang <zhanglongyang.cn@gmail.com>
2017-01-22 20:06:23 +1100

    TODO: type-safe Status

--- src/main/scala/model/Cell.scala
+++ src/main/scala/model/Cell.scala
@@ -2,6 +2,11 @@ package model
 
 import com.thoughtworks.binding.Binding.Var
 
+sealed trait Status
+case object Live extends Status
+case object Dead extends Status
+
+// TODO type-safe Status
 case class Cell(status: Var[String]) {
   def isLive: Boolean = this.status.get.eq("1")
 }
